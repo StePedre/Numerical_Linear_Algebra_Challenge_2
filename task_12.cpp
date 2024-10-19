b8179fd6a81a4063f42fa6615ef613bc7ce62b44
angelonotarnicola <anotarnicola@soldo.com>
2024-10-19 13:06:37 +0200

    Task 12

--- task_12.cpp
+++ task_12.cpp
@@ -16,27 +16,25 @@ Introduce a noise into the checkerboard image by adding random fluctuations of c
 ranging between [−50, 50] to each pixel. Export the resulting image in .png and upload it.
 */
 
-int main(int argc, char *argv[])
-{
-    std::srand(static_cast<unsigned int>(std::time(0)));
-    int height = 200;
-    int width = 200;
-    MatrixXd random = MatrixXd::Random(height, width);
+int main(int argc, char *argv[]){
+    int height = 3;
+    int width = 3;
+    MatrixXd C = MatrixXd::Random(3, 3);
+    MatrixXd D = MatrixXd::Random(3, 3);
 
-    random = random + (MatrixXd::Random(height, width) * 50);
+    MatrixXd CDT = C * D.transpose();
 
-
-    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> noise_image_char(height, width);
-    noise_image_char = random.unaryExpr([](double val) -> unsigned char
+    Matrix<unsigned char, Dynamic, Dynamic, RowMajor> CDT_image_char(height, width);
+    CDT_image_char = CDT.unaryExpr([](double val) -> unsigned char
                                { return static_cast<unsigned char>(max(0.0, min(val, 255.0))); });
 
-    const string output_noise_path = "output/noise_image.png";
-    if (stbi_write_png(output_noise_path.c_str(), width, height, 1, noise_image_char.data(), width) == 0)
+    const string output_CDT_path = "output/CDT_image.png";
+    if (stbi_write_png(output_CDT_path.c_str(), width, height, 1, CDT_image_char.data(), width) == 0)
     {
         cerr << "Error: Could not save noisy image" << endl;
         return 1;
     }
 
-    cout << "Noise image saved to " << output_noise_path << endl;
+    cout << "CDT image saved to " << output_CDT_path << endl;
 
 }
